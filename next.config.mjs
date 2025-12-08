e9684dd494f9bc34eafc81cc45f13902d813fd88
Vaibhav Shukla <keep.vaibin@gmail.com>
2025-12-08 04:08:28 -0800

    build

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const nextConfig = {
   // Tell Next.js to generate a static export into `out/`
   // when you run `next build`.
   output: "export",
+  images: {
+    unoptimized: true,
+  },
 
   // GitHub Pages serves this repo at:
   // https://keepvaibin.github.io/aminecraftserver
